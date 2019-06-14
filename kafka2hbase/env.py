5f74453d12808ed213b7c9f095c309c7fe7fd520
Craig Andrews <craig.andrews@infinityworks.com>
2019-06-14 16:02:34 +0100

    Add tests and types to config

--- kafka2hbase/env.py
+++ kafka2hbase/env.py
@@ -1,11 +1,11 @@
 """ Configuration and environment manipulation """
 
-import re
+from collections import namedtuple
 
 
 def delimited(d):
     """ Parse the value as a delimited string """
-    return lambda v: v.split(",") if v else d
+    return lambda v: [x for x in (v or d).split(",") if x]
 
 
 def string(d):
@@ -15,7 +15,7 @@ def string(d):
 
 def integer(d):
     """ Parse the value as an integer """
-    return lambda v: int(v) if v else d
+    return lambda v: int(v or d)
 
 
 def boolean(d):
@@ -23,65 +23,74 @@ def boolean(d):
     return lambda v: v.lower() == "true" if v else d
 
 
+Config = namedtuple("Config", ["kafka", "hbase"])
+
 default_config = {
-    "kafka.bootstrap_servers": delimited([]),
-    "kafka.client_id": string("kafka-to-hbase"),
-    "kafka.group_id": string(""),
-    "kafka.fetch_min_bytes": integer(1),
-    "kafka.fetch_max_wait_ms": integer(500),
-    "kafka.fetch_max_bytes": integer(50 * 1024 * 1024),
-    "kafka.max_partition_fetch_bytes": integer(1024 * 1024),
-    "kafka.request_timeout_ms": integer(305000),
-    "kafka.retry_backoff_ms": integer(100),
-    "kafka.reconnect_backoff_ms": integer(1000),
-    "kafka.max_in_flight_requests_per_connection": integer(5),
-    "kafka.check_crcs": boolean(True),
-    "kafka.metadata_max_age": integer(300000),
-    "kafka.session_timeout_ms": integer(10000),
-    "kafka.heartbeat_interval_ms": integer(3000),
-    "kafka.receive_buffer_bytes": integer(32 * 1024),
-    "kafka.send_buffer_bytes": integer(128 * 1024),
-    "kafka.consumer_timeout_ms": integer(0),
-    "kafka.conections_max_idle_ms": integer(540000),
-    "kafka.ssl": boolean(True),
-    "kafka.ssl_check_hostname": boolean(True),
-    "kafka.ssl_cafile": string(""),
-    "kafka.ssl_certfile": string(""),
-    "kafka.ssl_keyfile": string(""),
-    "kafka.ssl_password": string(""),
-    "kafka.ssl_crlfile": string(""),
-    "kafka.ssl_ciphers": string(""),
-    "kafka.sasl_kerberos": boolean(False),
-    "kafka.sasl_kerberos_service_name": string("kafka"),
-    "kafka.sasl_kerberos_domain_name": string(""),
-    "kafka.sasl_plain_username": string(""),
-    "kafka.sasl_plain_password": string(""),
-    "kafka.topics": delimited([]),
-    "hbase.host": string("localhost"),
-    "hbase.port": integer(9090),
-    "hbase.timeout": integer(0),
-    "hbase.namespace": string(""),
-    "hbase.table_prefix": string(""),
-    "hbase.column": string(""),
+    "kafka": {
+        "bootstrap_servers": delimited(""),
+        "client_id": string("kafka-to-hbase"),
+        "group_id": string(""),
+        "fetch_min_bytes": integer(1),
+        "fetch_max_wait_ms": integer(500),
+        "fetch_max_bytes": integer(50 * 1024 * 1024),
+        "max_partition_fetch_bytes": integer(1024 * 1024),
+        "request_timeout_ms": integer(305000),
+        "retry_backoff_ms": integer(100),
+        "reconnect_backoff_ms": integer(1000),
+        "max_in_flight_requests_per_connection": integer(5),
+        "check_crcs": boolean(True),
+        "metadata_max_age": integer(300000),
+        "session_timeout_ms": integer(10000),
+        "heartbeat_interval_ms": integer(3000),
+        "receive_buffer_bytes": integer(32 * 1024),
+        "send_buffer_bytes": integer(128 * 1024),
+        "consumer_timeout_ms": integer(0),
+        "conections_max_idle_ms": integer(540000),
+        "ssl": boolean(True),
+        "ssl_check_hostname": boolean(True),
+        "ssl_cafile": string(""),
+        "ssl_certfile": string(""),
+        "ssl_keyfile": string(""),
+        "ssl_password": string(""),
+        "ssl_crlfile": string(""),
+        "ssl_ciphers": string(""),
+        "sasl_kerberos": boolean(False),
+        "sasl_kerberos_service_name": string("kafka"),
+        "sasl_kerberos_domain_name": string(""),
+        "sasl_plain_username": string(""),
+        "sasl_plain_password": string(""),
+        "topics": delimited(""),
+    },
+    "hbase": {
+        "host": string("localhost"),
+        "port": integer(9090),
+        "timeout": integer(0),
+        "namespace": string(""),
+        "table_prefix": string(""),
+        "column": string(""),
+    },
 }
 
+KafkaConfig = namedtuple("KafkaConfig", default_config["kafka"].keys())  # type: ignore
+HbaseConfig = namedtuple("HbaseConfig", default_config["hbase"].keys())  # type: ignore
 
-def env_var_for_config_key(k):
+
+def env_var_for_config_key(g, k):
     """ Calculate the K2HB env var for a given config key """
-    return "K2HB_" + re.sub("\\W+", "_", k).upper()
+    return "K2HB_{}_{}".format(g.upper(), k.upper())
 
 
 def load_config(environment):
     """ Load environment variables into usable configuration from the given collection """
     config = {}
 
-    for path, factory in default_config.items():
-        env_var = env_var_for_config_key(path)
-        group, key = path.split(".", 1)
-        config[group] = config.get(group, {})
-        config[group][key] = factory(environment.get(env_var))
+    for group in default_config:
+        for key, factory in default_config[group].items():
+            env_var = env_var_for_config_key(group, key)
+            config[group] = config.get(group, {})
+            config[group][key] = factory(environment.get(env_var))
 
-    return config
+    return Config(kafka=KafkaConfig(**config["kafka"]), hbase=HbaseConfig(**config["hbase"]))
 
 
 def all_env_vars():
@@ -91,8 +100,10 @@ def all_env_vars():
         v = f(None)
         if isinstance(v, list):
             v = ",".join(v)
+        return v
 
     return {
-        env_var_for_config_key(k): _get_value(f)
-        for k, f in default_config.items()
+        env_var_for_config_key(g, k): _get_value(f)
+        for g in default_config
+        for k, f in default_config[g].items()
     }
