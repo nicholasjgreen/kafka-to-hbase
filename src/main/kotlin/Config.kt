c089219063b8b7110f8c37b24e6a7d73b6ac42a8
Daniel Chicot <dan.chicot@gmail.com>
2020-05-22 16:50:10 +0100

    Corrected typo. (#112)

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -39,7 +39,7 @@ object Config {
 
         val columnFamily = getEnv("K2HB_HBASE_COLUMN_FAMILY") ?: "cf"
         val columnQualifier = getEnv("K2HB_HBASE_COLUMN_QUALIFIER") ?: "record"
-        val retryMaxAttempts: Int = getEnv("K2HB_RETRY_MAX_ATTEMPS")?.toInt() ?: 3
+        val retryMaxAttempts: Int = getEnv("K2HB_RETRY_MAX_ATTEMPTS")?.toInt() ?: 3
         val retryInitialBackoff: Long = getEnv("K2HB_RETRY_INITIAL_BACKOFF")?.toLong() ?: 10000
         val retryBackoffMultiplier: Long = getEnv("K2HB_RETRY_BACKOFF_MULTIPLIER")?.toLong() ?: 2
     }
